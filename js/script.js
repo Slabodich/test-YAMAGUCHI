730239b02c64fbe8a1852349b18e0f2ebd44989e
Georgiy <egorslabodich1@gmail.com>
2022-12-05 00:42:38 +0300

    Upload project

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const upButton = document.querySelector(".table__up-btn");
+const downButton = document.querySelector(".table__down-btn");
+const upTablePart = document.querySelector(".table__top");
+
+let heightCounter = 0;
+
+upButton.addEventListener("click", function () {
+  if (heightCounter < 3) {
+    this.classList.remove("table__up-btn-disabled");
+    downButton.classList.remove("table__down-btn-disabled");
+    heightCounter++;
+    upTablePart.style.top = `${heightCounter * -10}px` ;
+    if (heightCounter >= 3) {
+      this.classList.add("table__up-btn-disabled");
+    }
+  } else {
+    this.classList.add("table__up-btn-disabled");
+  }
+});
+
+downButton.addEventListener("click", function () {
+  if (heightCounter > -3) {
+    this.classList.remove("table__down-btn-disabled");
+    upButton.classList.remove("table__up-btn-disabled");
+    heightCounter--;
+    upTablePart.style.top = `${heightCounter * -10}px` 
+    if (heightCounter <= -3) {
+      this.classList.add("table__down-btn-disabled");
+    }
+  } else {
+    this.classList.add("table__down-btn-disabled");
+  }
+});
